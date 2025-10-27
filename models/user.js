bd6f0ae96cddf094e7cc2f24d84e67d9c4de2a4e
IshitaSingh0822 <127220309+IshitaSingh0822@users.noreply.github.com>
2025-10-27 20:03:02 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,11 +4,30 @@ const Schema = mongoose.Schema;
 
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = new Schema({
+/*const userSchema = new Schema({
     email:{
         type: String,
         required: true
     }
 });
 userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', userSchema);*/
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+// Configure passport-local-mongoose options
+userSchema.plugin(passportLocalMongoose, {
+    usernameUnique: true,
+    findByUsername: function(model, queryParameters) {
+        // Add case insensitive search
+        queryParameters.username = new RegExp('^' + queryParameters.username + '$', 'i');
+        return model.findOne(queryParameters);
+    }
+});
+
 module.exports = mongoose.model('User', userSchema);
