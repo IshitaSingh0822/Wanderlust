c7021a252ac777d48efbea11cf1723f7e215e838
Ishita Singh <127220309+IshitaSingh0822@users.noreply.github.com>
2025-11-05 23:10:27 +0530

    Update cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,4 +1,4 @@
-/*const cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
@@ -24,36 +24,5 @@ const storage = new CloudinaryStorage({
 module.exports = {
     cloudinary,
     storage,
-};*/
-
-
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-});
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'wanderlust_DEV',
-    allowedFormats: ["png", "jpg", "jpeg"],
-    // MORE unique filename generation
-    public_id: (req, file) => {
-      const timestamp = Date.now();
-      const randomString = Math.random().toString(36).substring(2, 15);
-      const uniqueId = Math.floor(Math.random() * 1000000);
-      return `listing_${timestamp}_${randomString}_${uniqueId}`;
-    },
-  },
-});
-
-module.exports = {
-    cloudinary,
-    storage,
 };
-
 
